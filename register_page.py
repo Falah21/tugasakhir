7d1f9cb21af6979224569167bea611aa5078b55c
Ahmad Ihda Falah Annur <123763623+Falah21@users.noreply.github.com>
2026-05-19 12:35:59 +0700

    Update register_page.py

--- register_page.py
+++ register_page.py
@@ -5,12 +5,6 @@ from pymongo import MongoClient
 from css_style import load_css
 import bcrypt
 import time
-import os
-os.environ['TZ'] = 'Asia/Jakarta'
-try:
-    time.tzset()
-except:
-    pass
 
 # Optimasi koneksi MongoDB
 def get_mongo_client():
